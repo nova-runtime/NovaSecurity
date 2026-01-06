2cd2a488450f25f241e29a5f1c967c644330e90c
nova-runtime <maximilianzitzmann7@gmail.com>
2026-01-06 16:34:21 +0100

    Add book exploit protection (page & size limits)

--- src/main/java/de/nova/security/NovaSecurity.java
+++ src/main/java/de/nova/security/NovaSecurity.java
@@ -2,12 +2,18 @@ package de.nova.security;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.nova.security.listener.BookExploitListener;
+
 public final class NovaSecurity extends JavaPlugin {
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getLogger().info("NovaSecurity enabled");
+        
+        getServer().getPluginManager().registerEvents(
+                new BookExploitListener(this), this
+            );
     }
 
     @Override
