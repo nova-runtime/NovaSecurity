688d7bd96da8e94d07a7878abf0030c2d3b72e1c
nova-runtime <maximilianzitzmann7@gmail.com>
2026-01-06 16:40:42 +0100

    Improve main class with startup banner and clean logging

--- src/main/java/de/nova/security/NovaSecurity.java
+++ src/main/java/de/nova/security/NovaSecurity.java
@@ -1,28 +1,55 @@
 package de.nova.security;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
 import de.nova.security.listener.BookExploitListener;
 import de.nova.security.listener.NBTExploitListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NovaSecurity extends JavaPlugin {
 
+    private static NovaSecurity instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
+        long startTime = System.currentTimeMillis();
+
+        // Config
         saveDefaultConfig();
-        getLogger().info("NovaSecurity enabled");
-        
-        getServer().getPluginManager().registerEvents(
-                new BookExploitListener(this), this
-            );
-        
-        getServer().getPluginManager().registerEvents(
-                new NBTExploitListener(this), this
-            );
+
+        // Listener
+        Bukkit.getPluginManager().registerEvents(new BookExploitListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new NBTExploitListener(this), this);
+
+        long took = System.currentTimeMillis() - startTime;
+
+        // Startup banner
+        log("");
+        log("§b§lNovaSecurity §7v" + getDescription().getVersion());
+        log("§7by §fNova Runtime");
+        log("");
+        log("§a✔ Security systems loaded:");
+        log("§7• Book exploit protection");
+        log("§7• NBT / ItemMeta size protection");
+        log("");
+        log("§7Running on §f" + Bukkit.getName() + " §7(" + Bukkit.getVersion() + ")");
+        log("§7Startup completed in §f" + took + "ms");
+        log("");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("NovaSecurity disabled");
+        log("");
+        log("§cNovaSecurity disabled");
+        log("");
+    }
+
+    private void log(String message) {
+        Bukkit.getConsoleSender().sendMessage("§8[§bNovaSecurity§8] §r" + message);
+    }
+
+    public static NovaSecurity getInstance() {
+        return instance;
     }
 }
